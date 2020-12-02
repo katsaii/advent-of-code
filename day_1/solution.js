bba393ded11341447a2a0153ba755623ff1e9180
NuxiiGit <nuxii09@gmail.com>
2020-12-02 13:31:40 +0000

    updated day 1 solution console output

--- day_1/solution.js
+++ day_1/solution.js
@@ -9,7 +9,7 @@ fs.readFile("input", (err, data) => {
 			.map(x => parseInt(x, 10))
 			.filter(x => !isNaN(x));
 	const n = dates.length;
-	console.log("question 1 solutions");
+	console.log("expense report Z x Z");
 	for (let i = 0; i < n; i += 1) {
 		for (let j = i + 1; j < n; j += 1) {
 			const a = dates[i];
@@ -19,7 +19,7 @@ fs.readFile("input", (err, data) => {
 			}
 		}
 	}
-	console.log("\nquestion 2 solutions");
+	console.log("\nexpense report Z x Z x Z");
 	for (let i = 0; i < n; i += 1) {
 		for (let j = i + 1; j < n; j += 1) {
 			for (let k = j + 1; k < n; k += 1) {
