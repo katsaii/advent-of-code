375f060096f9bc07364471b04b5b28072b2528b5
NuxiiGit <nuxii09@gmail.com>
2020-12-04 01:39:36 +0000

    added part 2 for 2019 day 1 solution

--- 2019/day_1.hs
+++ 2019/day_1.hs
@@ -1,7 +1,21 @@
+fuelRequirement :: Integer -> Integer
+fuelRequirement n = if x < 2 then 0 else x - 2
+    where
+    x = n `div` 3
+
+fuelFuelRequirement :: Integer -> Integer
+fuelFuelRequirement n = if x == 0 then 0 else x + fuelFuelRequirement x
+    where
+    x = fuelRequirement n
+
+fuelSum :: [Integer] -> Integer
+fuelSum = foldr (+) 0
+
 main :: IO ()
 main = do
     input <- readFile "in/day_1.txt"
     let xs = map read $ words input :: [Integer]
-    let f = map (\n -> n `div` 3 - 2)
-    putStrLn "total fuel requirement"
-    putStrLn $ show $ foldr (+) 0 $ f xs
+    putStrLn "total fuel requirement (naive)"
+    putStrLn $ show $ fuelSum $ map fuelRequirement xs
+    putStrLn "\ntotal fuel requirement (corrected)"
+    putStrLn $ show $ fuelSum $ map fuelFuelRequirement xs
