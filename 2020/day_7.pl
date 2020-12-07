0c150b2d54d3ab284d9f8049a636857fd8a5b9c5
NuxiiGit <nuxii09@gmail.com>
2020-12-07 21:06:06 +0000

    got parsing working

--- 2020/day_7.pl
+++ 2020/day_7.pl
@@ -12,33 +12,36 @@ words(Atom, Words) :-
 	Space = "\s\t\r\n",
 	split_string(Atom, Space, Space, Words).
 
-abbreviate_rule(Long, Short) :-
-	Long = [Modifier, Colour, "bags", "contain", Number, InnerMod, InnerCol, "bags." | []],
-	Short = (Modifier, Colour, [(Number, InnerMod, InnerCol) | []]).
+abbreviate_rule(Words, Rule) :-
+	Words = [Modifier, Colour, "bags", "contain" | ContentsWords],
+	abbreviate_rule_contents(ContentsWords, Contents),
+	Rule = (Modifier, Colour, Contents).
+
+is_separator("bag,").
+is_separator("bags,").
+
+is_terminator("bag.").
+is_terminator("bags.").
+
+abbreviate_rule_contents(["no", "other", Term | []], []) :-
+	is_terminator(Term).
+abbreviate_rule_contents([CountAtom, Modifier, Colour, Term | []], Contents) :-
+	is_terminator(Term),
+	atom_number(CountAtom, Count),
+	Contents = [(Count, Modifier, Colour) | []].
+abbreviate_rule_contents([CountAtom, Modifier, Colour, Sep | Tail], Contents) :-
+	is_separator(Sep),
+	atom_number(CountAtom, Count),
+	abbreviate_rule_contents(Tail, OtherContents),
+	Contents = [(Count, Modifier, Colour) | OtherContents].
+
+parse_rule(Atom, Rule) :-
+	words(Atom, Words),
+	abbreviate_rule(Words, Rule).
 
 main :-
 	read_lines("in/day_7.txt", Lines),
-	maplist(words, Lines, Rules),
-	maplist(abbreviate_rule, Rules, AbbrRules),
-	write(AbbrRules), nl,
+	maplist(parse_rule, Lines, Rules),
+	write(Rules), nl,
 	halt.
 
-/*
-:- dynamic mother/2.
-
-grandmother(X, Y) :-
-	mother(X, Someone),
-	mother(Someone, Y).
-
-grandchildren(Grandmother, Grandchildren) :-
-	findall(Grandchild, grandmother(Grandmother, Grandchild), Grandchildren).
-
-main :-
-	assert(mother(alice, lisa)),
-	assert(mother(lisa, kate)),
-	assert(mother(lisa, bob)),
-	assert(mother(lisa, frank)),
-	grandchildren(alice, Xs),
-	write(Xs), nl,
-	halt.
-*/
