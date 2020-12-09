3951e34f96d13320859e2de99318c49acc8e7e22
NuxiiGit <nuxii09@gmail.com>
2020-12-09 22:27:24 +0000

    generalised

--- 2020/day_9.py
+++ 2020/day_9.py
@@ -1,27 +1,30 @@
-def find_sum_pairs(xs, n):
+def find_pair_sums(xs, n):
     return [(a, b) for a in xs for b in xs if a != b and a + b == n]
 
-def find_invalid_numbers(xs, offset):
-    return [xs[i] for i in range(offset, len(xs)) if not len(find_sum_pairs(xs[i - offset:i], xs[i]))]
+def find_contiguous_sums(xs, n):
+    sums = list()
+    while True:
+        limit = len(xs)
+        for start in range(0, limit):
+            if xs[start] == n:
+                continue
+            end = start
+            total = 0
+            while total < n and end < limit:
+                total += xs[end]
+                end += 1
+            if total == n:
+                sums.append(xs[start:end])
+        break
+    return sums
 
-def find_sum_contiguous(xs, n):
-    limit = len(xs)
-    for start in range(0, limit):
-        if xs[start] == n:
-            continue
-        end = start
-        total = 0
-        while total < n and end < limit:
-            total += xs[end]
-            end += 1
-        if total == n:
-            return xs[start:end]
-    return []
+def find_invalid_numbers(xs, offset):
+    return [xs[i] for i in range(offset, len(xs)) if not len(find_pair_sums(xs[i - offset:i], xs[i]))]
 
 content = open("in/day_9.txt").read()[:-1]
 cipher = [int(x) for x in content.split("\n")]
 preamble = 25
 invalid_number = find_invalid_numbers(cipher, preamble)[0]
-contiguous_sum = find_sum_contiguous(cipher, invalid_number)
+contiguous_sum = find_contiguous_sums(cipher, invalid_number)[0]
 encryption_weakness = min(contiguous_sum) + max(contiguous_sum)
 print(encryption_weakness)
