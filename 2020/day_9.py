e13bcec56ed0b7770d46cb3623a8cdffe87ae9e2
NuxiiGit <nuxii09@gmail.com>
2020-12-09 22:24:27 +0000

    day 9 part 2 solution

--- 2020/day_9.py
+++ 2020/day_9.py
@@ -4,8 +4,24 @@ def find_sum_pairs(xs, n):
 def find_invalid_numbers(xs, offset):
     return [xs[i] for i in range(offset, len(xs)) if not len(find_sum_pairs(xs[i - offset:i], xs[i]))]
 
+def find_sum_contiguous(xs, n):
+    limit = len(xs)
+    for start in range(0, limit):
+        if xs[start] == n:
+            continue
+        end = start
+        total = 0
+        while total < n and end < limit:
+            total += xs[end]
+            end += 1
+        if total == n:
+            return xs[start:end]
+    return []
+
 content = open("in/day_9.txt").read()[:-1]
 cipher = [int(x) for x in content.split("\n")]
 preamble = 25
-invalid_numbers = find_invalid_numbers(cipher, preamble)
-print(invalid_numbers)
+invalid_number = find_invalid_numbers(cipher, preamble)[0]
+contiguous_sum = find_sum_contiguous(cipher, invalid_number)
+encryption_weakness = min(contiguous_sum) + max(contiguous_sum)
+print(encryption_weakness)
