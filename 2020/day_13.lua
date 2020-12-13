b77fca5b125c9d28bbc7e39cee17ffa4a24ae98d
NuxiiGit <nuxii09@gmail.com>
2020-12-13 19:07:52 +0000

    bad solution

--- 2020/day_13.lua
+++ 2020/day_13.lua
@@ -22,32 +22,38 @@ function find_earliest_bus(depart, busses)
 	return earliest_bus_id, minimum_wait
 end
 
-function find_timespan(busses)
-	local start = 0
-	while true do
-		::search::
-		local limit = start
-		for i, bus_id in ipairs(busses) do
-			if bus_id == 0 then
-				limit = limit + 1
-			elseif limit % bus_id == 0 then
-				limit = limit + 1
-			else
-				local wait_time = bus_id - limit % bus_id
-				start = limit + wait_time - (i - 1)
-				goto search
+function find_furthest_jump(depart, busses)
+	local maximum_jump = 0
+	for i, bus_id in ipairs(busses) do
+		if bus_id ~= 0 then
+			local off = (depart + i - 1) % bus_id
+			if off ~= 0 then
+				local jump = bus_id - off
+				if jump > maximum_jump then
+					maximum_jump = jump
+				end
 			end
 		end
-		return start, limit
 	end
+	return maximum_jump
+end
+
+function find_timespan(busses)
+	local t = 1
+	repeat
+		local jump = find_furthest_jump(t, busses)
+		t = t + jump
+	until jump == 0
+	return t, t + #busses
 end
 
 file, _ = io.open("in/day_13.txt")
 depart = tonumber(file:read())
 busses = find_bus_ids(file:read())
 bus_id, wait = find_earliest_bus(depart, busses)
-start, limit = find_timespan(busses)
+start, limit = find_timespan(find_bus_ids("2,4,5,7,8,98,100,3000"))
+print(jump)
 print("the earliest bus multiplied by the number of minutes wait")
 print(("%d * %d = %d"):format(bus_id, wait, bus_id * wait))
-print("\nthe earliest timespan where busses depart at offsets matching their bus id")
+print("\nthe earliest timespan where busses depart at offsets matching their bus position")
 print(("%d -- %d"):format(start, limit))
