d834caa867e7843be8fc64602bfd0137c4a9d52c
NuxiiGit <nuxii09@gmail.com>
2020-12-13 17:22:21 +0000

    slow solution

--- 2020/day_13.lua
+++ 2020/day_13.lua
@@ -1,7 +1,8 @@
 function find_bus_ids(s)
 	local busses = { }
-	for bus_id in s:gmatch("([0-9]+),*") do
-		table.insert(busses, tonumber(bus_id))
+	for bus_id in s:gmatch("([^,]+),*") do
+		local val = (bus_id == "x") and 0 or tonumber(bus_id)
+		table.insert(busses, val)
 	end
 	return busses
 end
@@ -10,18 +11,43 @@ function find_earliest_bus(depart, busses)
 	local earliest_bus_id = nil
 	local minimum_wait = math.huge
 	for _, bus_id in ipairs(busses) do
-		local wait_time = bus_id - depart % bus_id
-		if wait_time < minimum_wait then
-			minimum_wait = wait_time
-			earliest_bus_id = bus_id
+		if bus_id ~= 0 then
+			local wait_time = bus_id - depart % bus_id
+			if wait_time < minimum_wait then
+				minimum_wait = wait_time
+				earliest_bus_id = bus_id
+			end
 		end
 	end
 	return earliest_bus_id, minimum_wait
+end
+
+function find_timespan(busses)
+	local start = 0
+	while true do
+		::search::
+		local limit = start
+		for i, bus_id in ipairs(busses) do
+			if bus_id == 0 then
+				limit = limit + 1
+			elseif limit % bus_id == 0 then
+				limit = limit + 1
+			else
+				local wait_time = bus_id - limit % bus_id
+				start = limit + wait_time - (i - 1)
+				goto search
+			end
+		end
+		return start, limit
+	end
 end
 
 file, _ = io.open("in/day_13.txt")
 depart = tonumber(file:read())
 busses = find_bus_ids(file:read())
 bus_id, wait = find_earliest_bus(depart, busses)
+start, limit = find_timespan(busses)
 print("the earliest bus multiplied by the number of minutes wait")
 print(("%d * %d = %d"):format(bus_id, wait, bus_id * wait))
+print("\nthe earliest timespan where busses depart at offsets matching their bus id")
+print(("%d -- %d"):format(start, limit))
