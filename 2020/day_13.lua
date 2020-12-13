f8390863400dedf92195daed00bb6a936a7191c3
NuxiiGit <nuxii09@gmail.com>
2020-12-13 16:32:55 +0000

    refactors

--- 2020/day_13.lua
+++ 2020/day_13.lua
@@ -1,12 +1,19 @@
+function find_bus_ids(s)
+	local busses = { }
+	for bus_id in s:gmatch("([0-9]+),*") do
+		table.insert(busses, tonumber(bus_id))
+	end
+	return busses
+end
+
 function find_earliest_bus(depart, busses)
-	earliest_bus_id = nil
-	minimum_wait = math.huge
-	for bus_id in busses:gmatch("([0-9]+),*") do
-		m = tonumber(bus_id)
-		wait_time = m - depart % m
+	local earliest_bus_id = nil
+	local minimum_wait = math.huge
+	for _, bus_id in ipairs(busses) do
+		local wait_time = bus_id - depart % bus_id
 		if wait_time < minimum_wait then
 			minimum_wait = wait_time
-			earliest_bus_id = m
+			earliest_bus_id = bus_id
 		end
 	end
 	return earliest_bus_id, minimum_wait
@@ -14,7 +21,7 @@ end
 
 file, _ = io.open("in/day_13.txt")
 depart = tonumber(file:read())
-busses = file:read()
+busses = find_bus_ids(file:read())
 bus_id, wait = find_earliest_bus(depart, busses)
-msg = "the earliest bus multiplied by the number of minutes wait\n%d * %d = %d"
-print(msg:format(bus_id, wait, bus_id * wait))
+print("the earliest bus multiplied by the number of minutes wait")
+print(("%d * %d = %d"):format(bus_id, wait, bus_id * wait))
