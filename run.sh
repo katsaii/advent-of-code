97c1c01a9acfdad47214eb8e5de909e1c75f35e3
NuxiiGit <nuxii09@gmail.com>
2020-12-14 17:59:57 +0000

    updated run script

--- run.sh
+++ run.sh
@@ -7,43 +7,50 @@ if [ "$FILEDIR" = "/" ]; then
 	echo 'nice try!'
 	exit 1
 fi
-if [ ! -d "$FILEDIR/bin" ]; then
-	mkdir "$FILEDIR/bin"
+(
+cd "$FILEDIR"
+BINDIR="./bin"
+OUTDIR="./out"
+if [ ! -d "$BINDIR" ]; then
+	mkdir "$BINDIR"
+fi
+if [ ! -d "$OUTDIR" ]; then
+	mkdir "$OUTDIR"
 fi
 IN="./$FILENAME.$FILEEXT"
-OUTDIR="./bin"
-OUT="$OUTDIR/$FILENAME.out"
-(cd "$FILEDIR"
+BIN="$BINDIR/$FILENAME.out"
+OUT="$OUTDIR/$FILENAME.txt"
 case $FILEEXT in
 	c)
-		clang -o "$OUT" "$IN"
-		"$OUT"
+		clang -o "$BIN" "$IN"
+		"$BIN" | tee "$OUT"
 		;;
 	hs)
-		ghc -o "$OUT" -odir "$OUTDIR/o" -hidir "$OUTDIR/h" "$IN"
-		"$OUT"
+		ghc -o "$BIN" -odir "$BINDIR/o" -hidir "$BINDIR/h" "$IN"
+		"$BIN" | tee "$OUT"
 		;;
 	js)
-		nodejs "$IN"
+		nodejs "$IN" | tee "$OUT"
 		;;
 	lua)
-		lua "$IN"
+		lua "$IN" | tee "$OUT"
 		;;
 	pl)
-		swipl -o "$OUT" -g main -c "$IN" 2> /dev/null
-		"$OUT"
+		swipl -o "$BIN" -g main -c "$IN" 2> /dev/null
+		"$BIN" | tee "$OUT"
 		;;
 	py)
-		python3 "$IN"
+		python3 "$IN" | tee "$OUT"
 		;;
 	rb)
-		ruby "$IN"
+		ruby "$IN" | tee "$OUT"
 		;;
 	rs)
-		rustc -o "$OUT" "$IN"
-		"$OUT"
+		rustc -o "$BIN" "$IN"
+		"$BIN" | tee "$OUT"
 		;;
 	*)
 		echo "unknown file extension .$FILEEXT"
 		;;
-esac)
+esac
+)
