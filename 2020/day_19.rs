a2886bc5cd70d7c0d36b4e0459cf0c9eb0353c68
NuxiiGit <nuxii09@gmail.com>
2020-12-21 01:56:45 +0000

    added matches function

--- 2020/day_19.rs
+++ 2020/day_19.rs
@@ -17,6 +17,37 @@ struct Grammar {
     productions : HashMap<usize, Production>
 }
 
+impl Grammar {
+    fn matches(&self, word : &str) -> bool {
+        matches!(self.check(0, word), Some(""))
+    }
+
+    fn check<'a>(&self, id : usize, word : &'a str) -> Option<&'a str> {
+        match self.productions.get(&id)? {
+            Production::Nonterminal(production) => {
+            'search:
+                for rule in production {
+                    let mut word_slice = word;
+                    for nonterminal in rule {
+                        if let Some(s) = self.check(*nonterminal, word_slice) {
+                            word_slice = s;
+                        } else {
+                            continue 'search;
+                        }
+                    }
+                    return Some(word_slice);
+                }
+            },
+            Production::Terminal(ch) => {
+                if word.chars().nth(0)? == *ch {
+                    return Some(&word[1..]);
+                }
+            }
+        }
+        None
+    }
+}
+
 impl From<&str> for Grammar {
     fn from(s : &str) -> Self {
         let mut productions = HashMap::new();
@@ -35,7 +66,6 @@ impl From<&str> for Grammar {
                         .collect::<Vec<Vec<usize>>>();
                 Production::Nonterminal(nonterminal)
             };
-            println!("{:?} {:?}", id, production);
             productions.insert(id, production);
         }
         Grammar { productions }
@@ -43,7 +73,22 @@ impl From<&str> for Grammar {
 }
 
 fn main() {
-    let content = fs::read_to_string("in/day_19.txt").unwrap();
+    let content = "
+0: 4 1 5
+1: 2 3 | 3 2
+2: 4 4 | 5 5
+3: 4 5 | 5 4
+4: \"a\"
+5: \"b\"
+
+yuh
+        ".to_string();
+        // fs::read_to_string("in/day_19.txt").unwrap();
     let (rules, words) = break_pair(content.trim(), "\n\n");
     let grammar = Grammar::from(rules);
+    println!("{:?}", grammar.matches("ababbb"));
+    println!("{:?}", grammar.matches("bababa"));
+    println!("{:?}", grammar.matches("abbbab"));
+    println!("{:?}", grammar.matches("aaabbb"));
+    println!("{:?}", grammar.matches("aaaabbb"));
 }
