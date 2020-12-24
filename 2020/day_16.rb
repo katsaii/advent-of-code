f653a33a84ff35ee8b640e5d3a7068064e4cc7ac
NuxiiGit <nuxii09@gmail.com>
2020-12-24 21:59:51 +0000

    so close

--- 2020/day_16.rb
+++ 2020/day_16.rb
@@ -1,4 +1,17 @@
-data = File.read("in/day_16.txt")
+data = <<~TICKETS #File.read("in/day_16.txt")
+    class: 1-3 or 5-7
+    row: 6-11 or 33-44
+    seat: 13-40 or 45-50
+
+    your ticket:
+    7,1,14
+
+    nearby tickets:
+    7,3,47
+    40,4,50
+    55,2,20
+    38,6,12
+TICKETS
 notes = data.split(/\n\n/)
 fields = { }
 notes[0].scan(/(.+): (\d+)-(\d+) or (\d+)-(\d+)/) do |x|
@@ -13,6 +26,7 @@ my_ticket = notes[1]
         .split(/\n/)[1]
         .split(/,/)
         .map{|x| x.to_i}
+ticket_fields = my_ticket.map{|_| fields.keys}
 tickets = notes[2]
         .split(/\n/)
         .drop(1)
@@ -20,7 +34,8 @@ tickets = notes[2]
                 .split(/,/)
                 .map{|x| x.to_i}}
 ticket_error_rate = 0
-tickets.each do |ticket|
+valid_tickets = tickets.filter do |ticket|
+    ticket_is_valid = true
     ticket.each do |value|
         catch :success do
             fields.each do |_, ranges|
@@ -31,7 +46,18 @@ tickets.each do |ticket|
                 end
             end
             ticket_error_rate += value
+            ticket_is_valid = false
         end
+    end
+    ticket_is_valid
+end
+valid_tickets.each do |ticket|
+    ticket.each_with_index do |value, col|
+        valid_fields = ticket_fields[col].filter do |key|
+            fields[key].any?{|min, max| value >= min and value <= max}
+        end
+        ticket_fields[col] -= valid_fields
     end
 end
 puts "ticket scanning error rate of nearby tickets\n#{ticket_error_rate}"
+puts "#{ticket_fields}"
