71272cb048463f993a5919087ffdff3611e08dac
NuxiiGit <nuxii09@gmail.com>
2020-12-26 22:36:39 +0000

    implemented game

--- 2020/day_22.hx
+++ 2020/day_22.hx
@@ -1,5 +1,52 @@
+import haxe.ds.List;
+import haxe.ds.Option;
+
+enum Turn {
+	A; B;
+}
+
 class Main {
+	static var playerA = new List<Int>();
+	static var playerB = new List<Int>();
+	static var turn = Turn.A;
+	static var winner = false;
+
+	static function play_turn() : Void {
+		switch ({ a : playerA.first(), b : playerB.first() }) {
+		case { a : null, b : _ } | { a : _, b : null }:
+			winner = true;
+		case { a : playA, b : playB }:
+			turn = turn == Turn.A ? Turn.B : Turn.A;
+			playerA.pop();
+			playerB.pop();
+			if (playA > playB) {
+				playerA.add(playA);
+				playerA.add(playB);
+			} else if (playB > playA) {
+				playerB.add(playB);
+				playerB.add(playA);
+			}
+		}
+	}
+
 	static function main() : Void {
-		Sys.println("hello world");
+		playerA.add(9);
+		playerA.add(2);
+		playerA.add(6);
+		playerA.add(3);
+		playerA.add(1);
+		playerB.add(5);
+		playerB.add(8);
+		playerB.add(4);
+		playerB.add(7);
+		playerB.add(10);
+		while (!winner) {
+			play_turn();
+		}
+		var winner_name = "player " + (turn == Turn.A ? "1" : "2");
+		Sys.println(winner_name + " is the winner");
+		for (item in playerB) {
+			Sys.println(item);
+		}
 	}
 }
