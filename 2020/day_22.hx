f10932c65798c109b32719017408a9fd942201ee
NuxiiGit <nuxii09@gmail.com>
2020-12-27 20:56:51 +0000

    refactors

--- 2020/day_22.hx
+++ 2020/day_22.hx
@@ -7,17 +7,31 @@ enum Winner {
 typedef Deck = List<Int>;
 
 class Main {
-	static var playerA = new Deck();
-	static var playerB = new Deck();
-	static var seenCombosA = new Map<String, Bool>();
-	static var seenCombosB = new Map<String, Bool>();
+	static function deckFromArray(arr : Array<Int>) : Deck {
+		var deck = new Deck();
+		for (card in arr) {
+			deck.add(card);
+		}
+		return deck;
+	}
 
-	static function playCombat() : Winner {
-		if (playerA.length == 0) {
-			return Winner.B;
-		} else if (playerB.length == 0) {
-			return Winner.A;
-		} else {
+	static function copyDeck(deck : Deck) : Deck {
+		var newDeck = new Deck();
+		for (card in deck) {
+			newDeck.add(card);
+		}
+		return newDeck;
+	}
+
+	static function playCombat(playerA : Deck, playerB : Deck) : { kind : Winner, deck : Deck } {
+		playerA = copyDeck(playerA);
+		playerB = copyDeck(playerB);
+		while (true) {
+			if (playerA.length == 0) {
+				return { kind : Winner.B, deck : playerB };
+			} else if (playerB.length == 0) {
+				return { kind : Winner.A, deck : playerA };
+			}
 			var playA = playerA.pop();
 			var playB = playerB.pop();
 			if (playA > playB) {
@@ -26,39 +40,6 @@ class Main {
 			} else if (playB > playA) {
 				playerB.add(playB);
 				playerB.add(playA);
-			}
-			return playCombat();
-		}
-	}
-
-	static function playCombatRecursive() : Winner {
-		if (playerA.length == 0) {
-			return Winner.B;
-		} else if (playerB.length == 0) {
-			return Winner.A;
-		} else {
-			var keyA = Std.string(playerA);
-			var keyB = Std.string(playerB);
-			if (seenCombosA.exists(keyA) && seenCombosB.exists(keyB)) {
-				return Winner.A;
-			}
-			Sys.println("Player 1's deck: " + playerA);
-			Sys.println("Player 2's deck: " + playerB);
-			seenCombosA.set(keyA, true);
-			seenCombosB.set(keyB, true);
-			var playA = playerA.pop();
-			var playB = playerB.pop();
-			if (playerA.length < playA && playerB.length < playB) {
-				return playCombatRecursive();
-			} else {
-				if (playA > playB) {
-					playerA.add(playA);
-					playerA.add(playB);
-				} else if (playB > playA) {
-					playerB.add(playB);
-					playerB.add(playA);
-				}
-				return playCombatRecursive();
 			}
 		}
 	}
@@ -72,41 +53,30 @@ class Main {
 		}
 		return score;
 	}
-
-	static function populateDecks(players : Array<Array<Int>>) : Void {
-		playerA.clear();
-		playerB.clear();
-		for (card in players[0]) {
-			playerA.add(card);
-		}
-		for (card in players[1]) {
-			playerB.add(card);
-		}
-	}
-
 	static function main() : Void {
 		var content = File.getContent("in/day_22.txt");
-		content = "Player 1:
-43
-19
+		/*content = "Player 1:
+9
+2
+6
+3
+1
 
 Player 2:
-2
-29
-14";
+5
+8
+4
+7
+10";*/
 		var players = (~/\n\n/g).split(content)
 				.map(function(x) return (~/\n/g).split(x)
 						.map(function(x) return Std.parseInt(x))
 						.filter(function(x) return x != null));
-		populateDecks(players);
-		var winner = playCombat();
-		var score = deckScore(winner == Winner.A ? playerA : playerB);
-		populateDecks(players);
-		var winner2 = playCombatRecursive();
-		var score2 = deckScore(winner2 == Winner.A ? playerA : playerB);
-		Sys.println("player " + (winner == Winner.A ? "1" : "2") + " is the winner");
+		var playerA = deckFromArray(players[0]);
+		var playerB = deckFromArray(players[1]);
+		var winner = playCombat(playerA, playerB);
+		var score = deckScore(winner.deck);
+		Sys.println("player " + (winner.kind == Winner.A ? "1" : "2") + " is the winner");
 		Sys.println(score);
-		Sys.println("\nplayer " + (winner2 == Winner.A ? "1" : "2") + " is the winner");
-		Sys.println(score2);
 	}
 }
