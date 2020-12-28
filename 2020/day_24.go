ed3ec0546cfd6e6f99fed4d4708ca7654a77ab20
NuxiiGit <nuxii09@gmail.com>
2020-12-28 21:57:01 +0000

    day 24 part 2 solution

--- 2020/day_24.go
+++ 2020/day_24.go
@@ -10,6 +10,15 @@ type Position struct {
 	X, Y int
 }
 
+func (pos Position) neighbours() [6]Position {
+	x := pos.X
+	y := pos.Y
+	return [6]Position {
+			Position { x + 1, y }, Position { x - 1, y },
+			Position { x, y + 1 }, Position { x, y - 1 },
+			Position { x + 1, y - 1 }, Position { x - 1, y + 1 } }
+}
+
 func follow_path(line string) Position {
 	x, y := 0, 0
 	for i := 0; i < len(line); i += 1 {
@@ -45,6 +54,31 @@ func follow_path(line string) Position {
 	return Position { x, y }
 }
 
+func mutate_tiles(tiles *map[Position]bool) {
+	neighbour_counts := make(map[Position]int)
+	for pos := range *tiles {
+		for _, neighbour := range pos.neighbours() {
+			if n, seen := neighbour_counts[neighbour]; seen {
+				neighbour_counts[neighbour] = n + 1
+			} else {
+				neighbour_counts[neighbour] = 1
+			}
+		}
+	}
+	new_tiles := make(map[Position]bool)
+	for neighbour, n := range neighbour_counts {
+		if _, seen := (*tiles)[neighbour]; seen {
+			if n == 0 || n > 2 {
+				continue
+			}
+		} else if n != 2 {
+			continue
+		}
+		new_tiles[neighbour] = true
+	}
+	*tiles = new_tiles
+}
+
 func main() {
 	file, _ := os.Open("in/day_24.txt")
 	defer file.Close()
@@ -60,4 +94,8 @@ func main() {
 		}
 	}
 	fmt.Printf("%d\n", len(tiles))
+	for i := 0; i < 100; i += 1 {
+		mutate_tiles(&tiles)
+		fmt.Printf("%d\n", len(tiles))
+	}
 }
