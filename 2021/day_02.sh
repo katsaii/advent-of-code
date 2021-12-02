f4e7e6968adccb0c0ea901aeff9cd76a82ac84ff
NuxiiGit <nuxii09@gmail.com>
2021-12-02 18:31:57 +0000

    added day 2 solutions

---
+++ 2021/day_02.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+x=0
+y=0
+aim=0
+
+function parse-instruction {
+	case "$1" in
+	forward)
+		x=$((x + $2))
+		y=$((y + $2 * aim))
+		;;
+	down)
+		aim=$((aim + $2))
+		;;
+	up)
+		aim=$((aim - $2))
+		;;
+	esac
+}
+
+while read -r line; do
+	parse-instruction $line
+done < "./in/day_02.txt"
+
+echo "product of final horizontal position ($x) and depth ($aim)"
+echo "$((x * aim))"
+echo
+echo "product of final horizontal position ($x) and corrected depth ($y)"
+echo "$((x * y))"
--- run.sh
+++ run.sh
@@ -43,91 +43,92 @@ fi
 BIN="$BINDIR/$FILENAME.out"
 OUT="$OUTDIR/$FILENAME.txt"
 case $FILEEXT in
-	c)
-		requires_command clang
-		clang -o "$BIN" "$IN"
-		"$BIN" | tee "$OUT"
-		;;
-	ck)
-		NOTE="Chuck can be installed at https://chuck.stanford.edu/release/" requires_command chuck
-		chuck "$IN" | tee "$OUT"
-		;;
-	cpp)
-		requires_command clang++
-		clang++ -o "$BIN" "$IN"
-		"$BIN" | tee "$OUT"
-		;;
-	erl)
-		requires_command erl
-		echo "not supported"
-		;;
-	go)
-		requires_command go
-		GO_OBJ="$BINDIR/o/$FILENAME.o"
-		go tool compile -o "$GO_OBJ" "$IN"
-		go tool link -o "$BIN" "$GO_OBJ"
-		"$BIN" | tee "$OUT"
-		;;
-	hs)
-		requires_command ghc
-		ghc -o "$BIN" -odir "$BINDIR/o" -hidir "$BINDIR/h" "$IN"
-		"$BIN" | tee "$OUT"
-		;;
-	hx)
-		requires_command haxe python3
-		HAXE_MAIN="$BINDIR/Main.hx"
-		HAXE_TARGET="$BINDIR/$FILENAME.py"
-		cp "$IN" "$HAXE_MAIN"
-		haxe -p "$BINDIR" --python "$HAXE_TARGET" --main Main
-		python3 "$HAXE_TARGET" | tee "$OUT"
-		;;
-	js)
-		requires_command node
-		node "$IN" | tee "$OUT"
-		;;
-	kats)
-		NOTE="KatScript can be installed at https://github.com/NuxiiGit/katscript-lang" requires_command katscript
-		katscript "$IN" | tee "$OUT"
-		;;
-	lua)
-		requires_command lua
-		lua "$IN" | tee "$OUT"
-		;;
-	ml)
-		requires_command ocamlc
-		echo "not supported"
-		;;
-	nim)
-		requires_command nim
-		echo "not supported"
-		;;
-	pl)
-		requires_command swipl
-		swipl -o "$BIN" -g main -c "$IN" 2> /dev/null
-		"$BIN" | tee "$OUT"
-		;;
-	py)
-		requires_command python3
-		python3 "$IN" | tee "$OUT"
-		;;
-	rb)
-		requires_command ruby
-		ruby "$IN" | tee "$OUT"
-		;;
-	rs)
-		requires_command rustc
-		rustc -o "$BIN" "$IN"
-		"$BIN" | tee "$OUT"
-		;;
-	sh)
-		"$IN" | tee "$OUT"
-		;;
-	zig)
-		requires_command zig
-		echo "not supported"
-		;;
-	*)
-		echo "unknown file extension .$FILEEXT"
-		;;
+c)
+	requires_command clang
+	clang -o "$BIN" "$IN"
+	"$BIN" | tee "$OUT"
+	;;
+ck)
+	NOTE="Chuck can be installed at https://chuck.stanford.edu/release/" requires_command chuck
+	chuck "$IN" | tee "$OUT"
+	;;
+cpp)
+	requires_command clang++
+	clang++ -o "$BIN" "$IN"
+	"$BIN" | tee "$OUT"
+	;;
+erl)
+	requires_command erl
+	echo "not supported"
+	;;
+go)
+	requires_command go
+	GO_OBJ="$BINDIR/o/$FILENAME.o"
+	go tool compile -o "$GO_OBJ" "$IN"
+	go tool link -o "$BIN" "$GO_OBJ"
+	"$BIN" | tee "$OUT"
+	;;
+hs)
+	requires_command ghc
+	ghc -o "$BIN" -odir "$BINDIR/o" -hidir "$BINDIR/h" "$IN"
+	"$BIN" | tee "$OUT"
+	;;
+hx)
+	requires_command haxe python3
+	HAXE_MAIN="$BINDIR/Main.hx"
+	HAXE_TARGET="$BINDIR/$FILENAME.py"
+	cp "$IN" "$HAXE_MAIN"
+	haxe -p "$BINDIR" --python "$HAXE_TARGET" --main Main
+	python3 "$HAXE_TARGET" | tee "$OUT"
+	;;
+js)
+	requires_command node
+	node "$IN" | tee "$OUT"
+	;;
+kats)
+	NOTE="KatScript can be installed at https://github.com/NuxiiGit/katscript-lang" requires_command katscript
+	katscript "$IN" | tee "$OUT"
+	;;
+lua)
+	requires_command lua
+	lua "$IN" | tee "$OUT"
+	;;
+ml)
+	requires_command ocamlc
+	echo "not supported"
+	;;
+nim)
+	requires_command nim
+	echo "not supported"
+	;;
+pl)
+	requires_command swipl
+	swipl -o "$BIN" -g main -c "$IN" 2> /dev/null
+	"$BIN" | tee "$OUT"
+	;;
+py)
+	requires_command python3
+	python3 "$IN" | tee "$OUT"
+	;;
+rb)
+	requires_command ruby
+	ruby "$IN" | tee "$OUT"
+	;;
+rs)
+	requires_command rustc
+	rustc -o "$BIN" "$IN"
+	"$BIN" | tee "$OUT"
+	;;
+sh)
+	chmod +x "$IN"
+	"$IN" | tee "$OUT"
+	;;
+zig)
+	requires_command zig
+	echo "not supported"
+	;;
+*)
+	echo "unknown file extension .$FILEEXT"
+	;;
 esac
 )
