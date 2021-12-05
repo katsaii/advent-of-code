f688c0d31ae551b20eeedde1d139950aa03f326c
NuxiiGit <nuxii09@gmail.com>
2021-12-05 22:44:24 +0000

    updated the languages summary

--- summaries/languages.md
+++ summaries/languages.md
@@ -2,19 +2,19 @@
 
 A table of programming languages used during the events, and how many solutions they were used for.
 
-| Language   | Count     |
-| ---------- | --------- |
-| Rust       | 5 `#####` |
-| Python     | 4 `####`  |
-| Ruby       | 4 `####`  |
-| Haskell    | 4 `####`  |
-| Go         | 2 `##`    |
-| Prolog     | 2 `##`    |
-| C          | 2 `##`    |
-| Julia      | 1 `#`     |
-| Shell      | 1 `#`     |
-| ChucK      | 1 `#`     |
-| Haxe       | 1 `#`     |
-| C++        | 1 `#`     |
-| Lua        | 1 `#`     |
-| JavaScript | 1 `#`     |
+| Language | Count | Tally |
+| --- | --- | --- |
+| Rust | 5 | [`#`](../2019/day_02.rs)[`#`](../2020/day_08.rs)[`#`](../2020/day_11.rs)[`#`](../2020/day_17.rs)[`#`](../2020/day_19.rs) |
+| Python | 4 | [`#`](../2020/day_05.py)[`#`](../2020/day_09.py)[`#`](../2020/day_10.py)[`#`](../2020/day_23.py) |
+| Ruby | 4 | [`#`](../2020/day_02.rb)[`#`](../2020/day_14.rb)[`#`](../2020/day_16.rb)[`#`](../2020/day_21.rb) |
+| Haskell | 4 | [`#`](../2019/day_01.hs)[`#`](../2020/day_04.hs)[`#`](../2020/day_06.hs)[`#`](../2020/day_18.hs) |
+| Go | 2 | [`#`](../2020/day_24.go)[`#`](../2021/day_04.go) |
+| Prolog | 2 | [`#`](../2020/day_07.pl)[`#`](../2021/day_03.pl) |
+| C | 2 | [`#`](../2020/day_03.c)[`#`](../2020/day_12.c) |
+| Julia | 1 | [`#`](../2021/day_05.jl) |
+| Shell | 1 | [`#`](../2021/day_02.sh) |
+| ChucK | 1 | [`#`](../2021/day_01.ck) |
+| Haxe | 1 | [`#`](../2020/day_22.hx) |
+| C++ | 1 | [`#`](../2020/day_15.cpp) |
+| Lua | 1 | [`#`](../2020/day_13.lua) |
+| JavaScript | 1 | [`#`](../2020/day_01.js) |
--- summaries/languages.rb
+++ summaries/languages.rb
@@ -1,28 +1,24 @@
 langs = []
+solutions = { }
 [2019, 2020, 2021].each do |year|
     page = File.read("#{year}.md")
-    page.scan(/\s*\|[^|]*Day[^|]*\|\s*([^|-]*?)\s*\|[^|]*\|/) do |x|
-        langs << x[0]
+    page.scan(/\s*\|[^|]*\[.+\]\((.*?)\)[^|]*\|\s*([^|-]*?)\s*\|[^|]*\|/) do |x|
+        solution = x[0]
+        lang = x[1]
+        langs << x[1]
+        solutions[lang] = [] if !solutions.has_key?(lang)
+        solutions[lang] << solution
     end
 end
-langs_tally = langs.tally.sort_by{|_, n| n}.reverse.map{|lang, n| [lang, "#{n} `#{"#" * n}`"]}
-max_lang_length = langs_tally.map{|lang, _| lang.length}.max
-max_tally_length = langs_tally.map{|_, n| n.length}.max
-out = <<~HEADER
-    # Language Summary
-
-    A table of programming languages used during the events, and how many solutions they were used for.
-HEADER
-lang_title = "Language"
-tally_title = "Count"
-lang_length = [max_lang_length, lang_title.length].max
-tally_length = [max_tally_length, tally_title.length].max
-out << "\n| #{lang_title}#{" " * (lang_length - lang_title.length)} "
-out << "| #{tally_title}#{" " * (tally_length - tally_title.length)} |"
-out << "\n| #{"-" * lang_length} | #{"-" * tally_length} |"
+langs_tally = langs.tally.sort_by{|_, n| n}.reverse
+out = "# Language Summary\n\nA table of programming languages used during the events, and how many solutions they were used for."
+out << "\n\n| Language | Count | Tally |\n| --- | --- | --- |"
 langs_tally.each do |lang, n|
-    out << "\n| #{lang}#{" " * (lang_length - lang.length)} "
-    out << "| #{n}#{" " * (tally_length - n.length)} |"
+    urls = ""
+    solutions[lang].each do |solution|
+        urls << "[`#`](#{solution})"
+    end
+    out << "\n| #{lang} | #{n} | #{urls} |"
 end
 out << "\n"
 File.write("languages.md", out)
