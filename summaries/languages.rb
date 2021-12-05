066202c454b9226f4dbcb5b832c14f6738c5f94c
NuxiiGit <nuxii09@gmail.com>
2021-12-05 01:15:48 +0000

    updated language summary

--- summaries/languages.rb
+++ summaries/languages.rb
@@ -5,16 +5,24 @@ langs = []
         langs << x[0]
     end
 end
-langs_tally = langs.tally.sort_by{|_, n| n}.reverse
+langs_tally = langs.tally.sort_by{|_, n| n}.reverse.map{|lang, n| [lang, "#{n} `#{"#" * n}`"]}
+max_lang_length = langs_tally.map{|lang, _| lang.length}.max
+max_tally_length = langs_tally.map{|_, n| n.length}.max
 out = <<~HEADER
-    # Programming Language Popularity
+    # Language Summary
 
     A table of programming languages used during the events, and how many solutions they were used for.
-
-    | Language | Solution Count |
-    | --- | --- |
 HEADER
+lang_title = "Language"
+tally_title = "Count"
+lang_length = [max_lang_length, lang_title.length].max
+tally_length = [max_tally_length, tally_title.length].max
+out << "\n| #{lang_title}#{" " * (lang_length - lang_title.length)} "
+out << "| #{tally_title}#{" " * (tally_length - tally_title.length)} |"
+out << "\n| #{"-" * lang_length} | #{"-" * tally_length} |"
 langs_tally.each do |lang, n|
-    out += "| #{lang} | #{n} |\n"
+    out << "\n| #{lang}#{" " * (lang_length - lang.length)} "
+    out << "| #{n}#{" " * (tally_length - n.length)} |"
 end
+out << "\n"
 File.write("languages.md", out)
