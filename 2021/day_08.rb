8627d0d80d1bff50731fc3d179bc9260d8838afa
NuxiiGit <nuxii09@gmail.com>
2021-12-23 23:53:03 +0000

    day 8 part 1

--- 2021/day_08.rb
+++ 2021/day_08.rb
@@ -1,2 +1,9 @@
 lines = File.readlines("in/day_08.txt", chomp: true)
 notes = lines.map{|line| line.split(" | ").map{|x| x.split(" ")}}
+unique_digit_count = 0
+notes.each do |_, output|
+    unique_digits = output.filter{|digit| [2, 3, 4, 7].include?(digit.length)}
+    unique_digit_count += unique_digits.length
+end
+puts "number of times the digits 1, 4, 7, and 8 appear"
+puts unique_digit_count
