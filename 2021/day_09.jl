0cad8495cee626b430a1751d8421f37acea30eca
NuxiiGit <nuxii09@gmail.com>
2021-12-25 12:02:35 +0000

    day 9 part 1

--- 2021/day_09.jl
+++ 2021/day_09.jl
@@ -1,4 +1,19 @@
 bytes = read("in/day_09.txt")
-width = findfirst(bytes .== 0x0a)
-numbers = reshape(map((byte) -> Int64(byte - 0x30), bytes[bytes .!= 0x0a]), width - 1, :)'
-println(numbers)
+samples = [Int64(byte - 0x30) for byte in bytes[bytes .!= 0x0a]]
+hm = reshape(samples, findfirst(bytes .== 0x0a) - 1, :)'
+height, width = size(hm)
+risk_level_sum = 0
+for i = 1:height
+    for j = 1:width
+        left = i == 1 ? typemax(Int64) : hm[i - 1, j]
+        top = j == 1 ? typemax(Int64) : hm[i, j - 1]
+        right = i == height ? typemax(Int64) : hm[i + 1, j]
+        bottom = j == width ? typemax(Int64) : hm[i, j + 1]
+        cell = hm[i, j]
+        if cell < left && cell < top && cell < right && cell < bottom
+            global risk_level_sum += hm[i, j] + 1
+        end
+    end
+end
+println("sum of risk levels of low points in the heightmap")
+println(risk_level_sum)
