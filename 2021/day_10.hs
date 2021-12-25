aac61b306ee578ddc2a427b09db9cf7f5e0f9efd
NuxiiGit <nuxii09@gmail.com>
2021-12-25 18:35:06 +0000

    day 10 part 2

--- 2021/day_10.hs
+++ 2021/day_10.hs
@@ -1,8 +1,7 @@
+import Data.Either (lefts, rights)
+import Data.List (sort)
 
-data ParseResult = Ok | Corrupted Char | Incomplete
-    deriving (Show)
-
-type Parser = String -> ParseResult
+type ParseResult = Either Char String
 
 getClosingParen :: Char -> Char
 getClosingParen '(' = ')'
@@ -14,32 +13,44 @@ getClosingParen _   = '\0'
 isOpeningParen :: Char -> Bool
 isOpeningParen = (/= '\0') . getClosingParen
 
-parse :: Parser
+parse :: String -> ParseResult
 parse = go []
     where
-    go expects [] = if null expects then Ok else Incomplete
+    go expects [] = Right expects
     go expects (x:xs)
         | isOpeningParen x = go (getClosingParen x:expects) xs
     go (expect:expects) (x:xs)
         | expect == x = go expects xs
-        | otherwise   = Corrupted x
+        | otherwise   = Left x
 
-parenScore :: Char -> Integer
-parenScore ')' = 3
-parenScore ']' = 57
-parenScore '}' = 1197
-parenScore '>' = 25137
+parseLines :: String -> [ParseResult]
+parseLines = map parse . lines
 
 syntaxErrorScore :: String -> Integer
-syntaxErrorScore = foldl (+) 0 . map parenScore . onlyCorrupted . lines
+syntaxErrorScore = foldl (+) 0 . map score . lefts . parseLines
     where
-    onlyCorrupted xs = [unwrap x | x <- map parse xs, isCorrupted x]
-    isCorrupted (Corrupted _) = True
-    isCorrupted _             = False
-    unwrap (Corrupted chr) = chr
+    score ')' = 3
+    score ']' = 57
+    score '}' = 1197
+    score '>' = 25137
+
+autocompleteScores :: String -> [Integer]
+autocompleteScores = map (foldl (\acc x -> acc * 5 + score x) 0) . rights . parseLines
+    where
+    score ')' = 1
+    score ']' = 2
+    score '}' = 3
+    score '>' = 4
+
+autocompleteScore :: String -> Integer
+autocompleteScore src = sort scores !! (length scores `div` 2)
+    where
+    scores = autocompleteScores src
 
 main :: IO ()
 main = do
     input <- readFile "in/day_10.txt"
-    putStrLn $ "sum of answers for basic maths homework"
+    putStrLn $ "syntax checker score"
     putStrLn $ show $ syntaxErrorScore input
+    putStrLn $ "\nautocomplete score"
+    putStrLn $ show $ autocompleteScore input
