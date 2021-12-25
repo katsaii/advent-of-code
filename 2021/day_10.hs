be64027b0b907b9bf58062b40a33d85a15a0cfb0
NuxiiGit <nuxii09@gmail.com>
2021-12-25 17:54:16 +0000

    day 10 part 1

--- 2021/day_10.hs
+++ 2021/day_10.hs
@@ -24,5 +24,22 @@ parse = go []
         | expect == x = go expects xs
         | otherwise   = Corrupted x
 
+parenScore :: Char -> Integer
+parenScore ')' = 3
+parenScore ']' = 57
+parenScore '}' = 1197
+parenScore '>' = 25137
+
+syntaxErrorScore :: String -> Integer
+syntaxErrorScore = foldl (+) 0 . map parenScore . onlyCorrupted . lines
+    where
+    onlyCorrupted xs = [unwrap x | x <- map parse xs, isCorrupted x]
+    isCorrupted (Corrupted _) = True
+    isCorrupted _             = False
+    unwrap (Corrupted chr) = chr
+
 main :: IO ()
-main = putStrLn $ show $ parse "<{([([[(<>()){}]>(<<{{"
+main = do
+    input <- readFile "in/day_10.txt"
+    putStrLn $ "sum of answers for basic maths homework"
+    putStrLn $ show $ syntaxErrorScore input
