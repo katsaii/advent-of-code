90450a3d14ce6ef9ffcda1fc2a352cd014205bc0
NuxiiGit <nuxii09@gmail.com>
2021-12-25 22:59:45 +0000

    refactor

--- 2021/day_12.pl
+++ 2021/day_12.pl
@@ -16,22 +16,28 @@ read_caves_(Stream) :-
 connected(X, Y) :- cave(X, Y).
 connected(X, Y) :- cave(Y, X).
 
-visitableOnce(Node, Visited) :-
-	string_chars(Node, [Fst | _]),
-	char_type(Fst, lower),
-	memberchk(Node, Visited).
+is_upper(X) :-
+	string_chars(X, [Fst | _]),
+	char_type(Fst, upper).
+
+visitable(Node, _, _) :- is_upper(Node).
+visitable(Node, Pred, Visited) :-
+	\+ is_upper(Node),
+	call(Pred, Node, Visited).
 
 path(Start, End, Pred, Path) :- path_(Start, End, Pred, [Start], Path).
 
 path_(End, End, _, Visited, Path) :- reverse(Visited, Path).
 path_(Start, End, Pred, Visited, Path) :-
 	connected(Start, Next),
-	\+ call(Pred, Next, Visited),
+	visitable(Next, Pred, Visited),
 	path_(Next, End, Pred, [Next | Visited], Path).
+
+enterOnce(Node, Visited) :- \+ member(Node, Visited).
 
 main(_) :-
 	read_caves("in/day_12.txt"),
-	findall(Path, path("start", "end", visitableOnce, Path), Paths),
-	length(Paths, Len),
-	write("number of paths out of the cave system"), nl,
-	write(Len), nl.
+	findall(Path, path("start", "end", enterOnce, Path), OncePaths),
+	length(OncePaths, OnceLen),
+	write("number of paths through the cave system"), nl,
+	write(OnceLen), nl.
