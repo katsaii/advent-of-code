e2ee043b17c4b4def14797ff1834bf764c9d6493
NuxiiGit <nuxii09@gmail.com>
2021-12-28 22:02:03 +0000

    refactored solution in preparation for the beast

--- 2021/day_15.py
+++ 2021/day_15.py
@@ -16,41 +16,45 @@ class Node:
     def __lt__(self, other):
         return self.weight < other.weight
 
-lines = open("in/day_15.txt").read().splitlines()
-nodes = [
-    [Node.new(row, col, int(c)) for (col, c) in enumerate(line)]
-    for (row, line) in enumerate(lines)
-]
-height, width = len(nodes), len(nodes[0])
-start = nodes[0][0]
-dest = nodes[-1][-1]
-start.weight = 0
-start.inserted = True
-queue = [start]
-while queue:
-    node = heapq.heappop(queue)
-    if node == dest:
-        break
-    node.visited = True
-    row, col = node.pos
-    neighbours_pos = [
-        (row - 1, col), (row + 1, col),
-        (row, col - 1), (row, col + 1),
+def search(risk_map):
+    nodes = [
+        [Node.new(row, col, risk) for (col, risk) in enumerate(line)]
+        for (row, line) in enumerate(risk_map)
     ]
-    for (nrow, ncol) in neighbours_pos:
-        if nrow in { -1, height } or ncol in { -1, width }:
-            continue
-        neighbour = nodes[nrow][ncol]
-        if neighbour.visited:
-            continue
-        new_weight = node.weight + neighbour.risk
-        if new_weight < neighbour.weight:
-            neighbour.weight = new_weight
-            neighbour.parent = node
-            if not neighbour.inserted:
-                queue.append(neighbour)
-                dirty = True
-    if dirty:
-        heapq.heapify(queue)
+    height, width = len(nodes), len(nodes[0])
+    start = nodes[0][0]
+    dest = nodes[-1][-1]
+    start.weight = 0
+    start.inserted = True
+    queue = [start]
+    while queue:
+        node = heapq.heappop(queue)
+        if node == dest:
+            break
+        node.visited = True
+        row, col = node.pos
+        neighbours_pos = [
+            (row - 1, col), (row + 1, col),
+            (row, col - 1), (row, col + 1),
+        ]
+        for (nrow, ncol) in neighbours_pos:
+            if nrow in { -1, height } or ncol in { -1, width }:
+                continue
+            neighbour = nodes[nrow][ncol]
+            if neighbour.visited:
+                continue
+            new_weight = node.weight + neighbour.risk
+            if new_weight < neighbour.weight:
+                neighbour.weight = new_weight
+                neighbour.parent = node
+                if not neighbour.inserted:
+                    queue.append(neighbour)
+                    dirty = True
+        if dirty:
+            heapq.heapify(queue)
+    return dest.weight
+
+lines = open("in/day_15.txt").read().splitlines()
+risks = [[int(c) for c in line] for line in lines]
 print("total risk level of the journey")
-print(dest.weight)
+print(search(risks))
