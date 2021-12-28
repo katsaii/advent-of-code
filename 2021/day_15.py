5ba098b7b635916289f826e64e43dbd2435610bd
NuxiiGit <nuxii09@gmail.com>
2021-12-28 21:48:35 +0000

    day 15 part 1

---
+++ 2021/day_15.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+import heapq
+
+@dataclass
+class Node:
+    pos : (int, int)
+    risk : int
+    visited : bool
+    parent : ...
+    weight : int
+
+    def new(row, col, risk):
+        return Node((row, col), risk, False, None, float('inf'))
+
+    def __lt__(self, other):
+        return self.weight < other.weight
+
+lines = open("in/day_15.txt").read().splitlines()
+nodes = [
+    [Node.new(row, col, int(c)) for (col, c) in enumerate(line)]
+    for (row, line) in enumerate(lines)
+]
+height, width = len(nodes), len(nodes[0])
+queue = [node for row in nodes for node in row]
+heapq.heapify(queue)
+start = nodes[0][0]
+dest = nodes[-1][-1]
+start.weight = 0
+while queue:
+    node = heapq.heappop(queue)
+    if node == dest:
+        break
+    node.visited = True
+    row, col = node.pos
+    neighbours_pos = [
+        (row - 1, col), (row + 1, col),
+        (row, col - 1), (row, col + 1),
+    ]
+    dirty = False
+    for (nrow, ncol) in neighbours_pos:
+        if nrow in { -1, height } or ncol in { -1, width }:
+            continue
+        neighbour = nodes[nrow][ncol]
+        if neighbour.visited:
+            continue
+        new_weight = node.weight + neighbour.risk
+        if new_weight < neighbour.weight:
+            neighbour.weight = new_weight
+            neighbour.parent = node
+            dirty = True
+    if dirty:
+        heapq.heapify(queue)
+print("total risk level of the journey")
+print(dest.weight)
