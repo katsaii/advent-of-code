d8253066ce69d618e19babbfe564630d04cbdf0d
NuxiiGit <nuxii09@gmail.com>
2022-12-03 18:47:40 +0000

    day 3 part 2

--- 2022/day_03.rb
+++ 2022/day_03.rb
@@ -5,10 +5,19 @@ end
 total = 0
 rucksacks.each do |rucksack|
     lhs, rhs = rucksack.each_slice((rucksack.size / 2).round).to_a
-    dup = lhs & rhs
-    dup.each do |duplicate|
+    duplicates = lhs & rhs
+    duplicates.each do |duplicate|
         total += duplicate + 1
     end
 end
-
+total_badges = 0
+rucksacks.each_slice(3) do |rucksack1, rucksack2, rucksack3|
+    duplicates = rucksack1 & rucksack2 & rucksack3
+    duplicates.each do |duplicate|
+        total_badges += duplicate + 1
+    end
+end
+puts "sum of priorities of duplicate items in each rucksack"
 puts total
+puts "\nsum of priorities of badges"
+puts total_badges
