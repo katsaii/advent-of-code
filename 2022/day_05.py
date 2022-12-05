59b47a9cee017e5593aecbd4bf48b11b3ef2deaa
NuxiiGit <nuxii09@gmail.com>
2022-12-05 23:21:59 +0000

    day 5 part 2

--- 2022/day_05.py
+++ 2022/day_05.py
@@ -17,8 +17,19 @@ insts = [
     [int(item) for item in re.split("[^0-9]", inst) if item]
     for inst in lines_tail
 ]
+import copy
+stacks_9000 = copy.deepcopy(stacks)
 for [n, from_, to] in insts:
     for _ in range(n):
-        stacks[to - 1].append(stacks[from_ - 1].pop())
-top_values = "".join([items[-1] for items in stacks])
-print(top_values)
+        stacks_9000[to - 1].append(stacks_9000[from_ - 1].pop())
+stacks_9001 = copy.deepcopy(stacks)
+for [n, from_, to] in insts:
+    for offset in range(n)[::-1]:
+        src_stack = stacks_9001[from_ - 1]
+        stacks_9001[to - 1].append(src_stack[-1 - offset])
+        del src_stack[-1 - offset]
+[top_9000, top_9001] = [
+    "".join([items[-1] for items in stacks])
+    for stacks in [stacks_9000, stacks_9001]
+]
+print(top_9000, top_9001)
