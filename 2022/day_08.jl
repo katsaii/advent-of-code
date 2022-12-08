bc014a36ed1f8f5e1b44bafef6e14b265dbfc012
NuxiiGit <nuxii09@gmail.com>
2022-12-08 21:54:31 +0000

    day 8 part 2

--- 2022/day_08.jl
+++ 2022/day_08.jl
@@ -1,19 +1,37 @@
 trees = permutedims(hcat(map(collect, readlines("in/day_08.txt"))...))
 height, width = size(trees)
 visible_count = 0
+max_scenic_score = 0
 for i = 1:height
 	for j = 1:width
 		tree = trees[i, j]
 		neighbours = [
-			trees[i, begin:(j - 1)],
+			trees[i, (j - 1):-1:begin],
 			trees[i, (j + 1):end],
-			trees[begin:(i - 1), j],
+			trees[(i - 1):-1:begin, j],
 			trees[(i + 1):end, j],
 		]
 		visible = any(map(x -> all(map(<(tree), x)), neighbours))
 		if visible
 			global visible_count += 1
 		end
+		function view_dist(trees)
+			local n = 0
+			for idx = eachindex(trees)
+				n += 1
+				if trees[idx] >= tree
+					break
+				end
+			end
+			n
+		end
+		scenic_score = prod(map(view_dist, neighbours))
+		if scenic_score > max_scenic_score
+			global max_scenic_score = scenic_score
+		end
 	end
 end
-@show visible_count
+println("total number of visible trees from the edges of the forest")
+println(visible_count)
+println("\nmaximum scenic score out of trees in the forest")
+println(max_scenic_score)
