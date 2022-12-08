9de806c956ba7e31a2a06a1c91922a7843688bd8
NuxiiGit <nuxii09@gmail.com>
2022-12-08 19:18:52 +0000

    improved day 7 a bit

--- 2022/day_07.rb
+++ 2022/day_07.rb
@@ -1,42 +1,23 @@
-def file_create(name, size, parent)
-    file = {
-        :name => name,
-        :size => size,
-        :children => { },
-        :parent => parent,
-    }
-    if parent != nil
-        parent[:children][name] = file
-        while parent != nil
-            parent[:size] += size
-            parent = parent[:parent]
-        end
-    end
-    file
-end
-
 insts = File.readlines("in/day_07.txt", chomp: true).map{ |x| x.split(" ") }
-root = file_create("/", 0, nil)
-cwd = root
-dirs = [root]
+dirs = { "/" => 0 }
+paths = dirs.keys
 insts.each do |inst|
     case inst
     in ["$", "cd", "/"]
-        cwd = root
+        paths = ["/"]
     in ["$", "cd", ".."]
-        cwd = cwd[:parent]
+        paths.pop
     in ["$", "cd", path]
-        cwd = cwd[:children][path]
+        paths.push("#{paths[-1]}#{path}/")
     in ["$", "ls"]
     in ["dir", name]
-        dirs << file_create(name, 0, cwd)
+        dirs["#{paths[-1]}#{name}/"] = 0
     in [size, name]
-        file_create(name, size.to_i, cwd)
+        paths.each{ |path| dirs[path] += size.to_i }
     end
 end
-dir_sizes = dirs.map{ |x| x[:size] }.sort
-total_size = root[:size]
-unused_space = 70000000 - total_size
+dir_sizes = dirs.values.sort
+unused_space = 70000000 - dirs["/"]
 small_total_size = dir_sizes.take_while{ |x| x < 100000 }.sum
 delete_size = dir_sizes.select{ |x| unused_space + x >= 30000000 }.sort[0]
 puts "total size of directories smaller than 100000"
