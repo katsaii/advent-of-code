614a658b1a936711e926fb38bbd75687400ecfa2
NuxiiGit <nuxii09@gmail.com>
2022-12-12 18:34:36 +0000

    day 12 part 2

--- 2022/day_12.jl
+++ 2022/day_12.jl
@@ -9,24 +9,39 @@ while !isempty(frontier)
 	deleteat!(frontier, 1)
 	neighbours = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 	for (dy, dx) in neighbours
-		if y + dy < 1 || x + dx < 1 || y + dy > height || x + dx > width || (y + dy, x + dx) in edges.keys
+		y2, x2 = y + dy, x + dx
+		if y2 < 1 || x2 < 1 || y2 > height || x2 > width || (y2, x2) in edges.keys
 			continue
 		end
-		function convert_signal(v)
-			return v == 'S' ? 'a' : v == 'E' ? 'z' : v
-		end
-		curr = field[y, x]
-		dest = field[y + dy, x + dx]
-		if convert_signal(dest) + 1 < convert_signal(curr)
+		convert_signal(v) = v == 'S' ? 'a' : v == 'E' ? 'z' : v
+		if convert_signal(field[y2, x2]) + 1 < convert_signal(field[y, x])
 			continue
 		end
-		edges[(y + dy, x + dx)] = (y, x)
-		push!(frontier, (y + dy, x + dx))
+		edges[(y2, x2)] = (y, x)
+		push!(frontier, (y2, x2))
 	end
 end
-steps = 0
-while edges[start] != (-1, -1)
-	global steps += 1
-	global start = edges[start]
+function path_length(dest)
+	steps = 0
+	while haskey(edges, dest) && edges[dest] != (-1, -1)
+		steps += 1
+		dest = edges[dest]
+	end
+	steps, dest
 end
-@show steps
+shortest_path = typemax(Int)
+for y = 1:height
+	for x = 1:width
+		if field[y, x] != 'a'
+			continue
+		end
+		length, root = path_length((y, x))
+		if length < shortest_path && root == exit
+			global shortest_path = length
+		end
+	end
+end
+println("shortest path between the start and end of the trail")
+println(path_length(start)[1])
+println("\nshortest possible path from the bottom of the mountain to the end of the trail")
+println(shortest_path)
