b76b0c6f82b32d4eeb5ed37f81a78c2d7acfec0e
NuxiiGit <nuxii09@gmail.com>
2022-12-14 22:33:19 +0000

    day 13 part 2

--- 2022/day_13.js
+++ 2022/day_13.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 fs.readFile("in/day_13.txt", (_, data) => {
 	const packets = data.toString().split("\n\n")
-		.map(packet => packet.split("\n").map(line => eval(line)));
+		.map(packet => packet.split("\n")
+			.filter(line => line).map(line => eval(line)));
 	const compare = function(lhs, rhs) {
 		switch (typeof lhs + typeof rhs) {
 		case "numbernumber":
@@ -20,5 +21,11 @@ fs.readFile("in/day_13.txt", (_, data) => {
 	};
 	const sum = Array.from(packets.entries()).reduce(
 		(acc, [i, [lhs, rhs]]) => acc + (compare(lhs, rhs) < 1 ? i + 1 : 0), 0);
+	const sorted = packets.flat().concat([[[2]], [[6]]]).sort(compare);
+	const dividerIndex = x => 1 + sorted.findIndex(elem =>
+		typeof elem == "object" && typeof elem[0] == "object" && elem[0] == x);
+	console.log("sum of the indices for correctly sorted pairs")
 	console.log(sum);
+	console.log("\ndecoder key");
+	console.log(dividerIndex(2) * dividerIndex(6));
 });
